aeed08d2a8d4f8e09f1a223eb1e33fe48ee9de2e
senyoudev <younesmeskafe2018@gmail.com>
2024-04-19 23:27:18 +0100

    a test model

--- src/model.rs
+++ src/model.rs
@@ -1,7 +1,12 @@
 // A model is a struct that represents a collection in no-sql database(It is the equivalent of a table in a relational database).
 // The model is used to interact with the database. 
 
+use serde::{Serialize, Deserialize};
+
+// Define a user document model to Test the connection to the database
 #[derive(Debug, Serialize, Deserialize)]
-struct Document<T> {
-    data: T,
+pub struct User {
+    pub username: String,
+    pub email: String,
+    pub age: u32,
 }
